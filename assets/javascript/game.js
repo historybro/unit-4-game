08be9cff05e53d741a0d6fcae3241802c47c9c16
GSM <25423602+historybro@users.noreply.github.com>
2019-02-24 21:33:43 -0600

    Git Maybe final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,71 @@
 $(document).ready(function () {
-    var targetNumber = Math.floor(Math.random() * 102) + 19;
-    var numberOptions = [];
+    //initalize variables etc
+    var targetNumber = 0;
+    var scoreOptions = [];
     var counter = 0;
     var wins = 0;
     var loss = 0;
+//pushes variables to html
     $("#wins").text(wins);
     $("#loss").text(loss);
     $("#score").text(counter);
     $("#number-to-guess").text(targetNumber);
     reset();
 
+//function called to reset game
     function reset() {
         targetNumber = Math.floor(Math.random() * 102) + 19;
-        numberOptions = [];
+        scoreOptions = [];
         counter = 0;
         $("#number-to-guess").text(targetNumber);
         $("#score").text(counter)
-        crystalNumbers();
+        crystalScore();
         console.log("targetNumber: " + targetNumber);
-        console.log("numberOptions: " + numberOptions);
+        console.log("scoreOptions: " + scoreOptions);
     }
 
-    function crystalNumbers() {
-        for (var i = 0; i < 4; i++) {
-            randomAnswer = Math.floor(Math.random() * 12 + 1);
-            numberOptions.push(randomAnswer);
-        }
-    }    
-    
-    for (var i = 0; i < numberOptions.length; i++) {
-        var imageCrystal = $("<img>");
-        imageCrystal.addClass("crystal-image");        
-        imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
-        imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-        $("#crystals").append(imageCrystal);
+//function called on win, adds to total # of wins, resets game
+    function wincon() {
+        wins++;
+        $("#wins").text(wins);
+        reset();
+    }
+
+//function called on loss, adds to total # of losses, resets game
+    function losecon() {
+        loss++;
+        $("#loss").text(loss);
+        reset();
+    }
+
+    function crystalScore() {
+        //old code for creating random scores
+        // for (var i = 0; i < 4; i++) {
+        //     randomAnswer = Math.floor(Math.random() * 12 + 1);
+        //     if ()
+        //     scoreOptions.push(randomAnswer);
+        //     console.log(randomAnswer + " :Random Answer");
+        //     console.log(scoreOptions + " :Number Options");
+
+        // }
+        //new code creates random non repeating numbers
+        let scoreOptions = [];
+
+        let scoreGenerator = function (arr) {
+            if (arr.length >= 4) return;
+            let crystalScore = Math.floor(Math.random() * 12 + 1);
+            if (arr.indexOf(crystalScore) < 0) {
+                arr.push(crystalScore);
+            }
+            scoreGenerator(arr);
+        };
+
+        scoreGenerator(scoreOptions);
+        console.log("NumberOptions: " + scoreOptions);
+        $("#crystal1").attr("data-crystalvalue", scoreOptions[0]);
+        $("#crystal2").attr("data-crystalvalue", scoreOptions[1]);
+        $("#crystal3").attr("data-crystalvalue", scoreOptions[2]);
+        $("#crystal4").attr("data-crystalvalue", scoreOptions[3]);
     }
 
     $(".crystal-image").on("click", function () {
@@ -43,13 +75,9 @@ $(document).ready(function () {
         counter += crystalValue;
         $("#score").text(counter);
         if (counter == targetNumber) {
-            wins++;
-            $("#wins").text(wins);
-            reset();
+            wincon();
         } else if (counter >= targetNumber) {
-            loss++;
-            $("#loss").text(loss);
-            reset();
+            losecon();
         }
 
     });
